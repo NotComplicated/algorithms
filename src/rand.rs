44629b6e9864206f08146e49f87d16a49fbdafe7
Andres Dejesus <andresdejesus123@gmail.com>
2024-11-25 18:50:21 -0600

    insertion sort

--- src/rand.rs
+++ src/rand.rs
@@ -2,17 +2,22 @@ use rand::{
     distributions::{uniform::SampleUniform, Alphanumeric, DistString},
     prelude::*,
 };
-use std::{
-    array,
-    cell::RefCell,
-    ops::{Range, RangeInclusive},
-};
+use std::{array, cell::RefCell, ops::Range};
 
-const ARRAY_SIZE: usize = 20;
-const STRING_SIZE: RangeInclusive<usize> = 1..=50;
-
-pub fn array_of<T>(mut randomizer: impl FnMut() -> T) -> [T; ARRAY_SIZE] {
+fn array_of<T, const N: usize>(mut randomizer: impl FnMut() -> T) -> [T; N] {
     array::from_fn(|_| randomizer())
+}
+
+pub fn ten_of<T>(randomizer: impl FnMut() -> T) -> [T; 10] {
+    array_of(randomizer)
+}
+
+pub fn hundred_of<T>(randomizer: impl FnMut() -> T) -> [T; 100] {
+    array_of(randomizer)
+}
+
+pub fn thousand_of<T>(randomizer: impl FnMut() -> T) -> [T; 1000] {
+    array_of(randomizer)
 }
 
 thread_local! {
@@ -25,7 +30,7 @@ pub fn int() -> i32 {
 
 pub fn string() -> String {
     RNG.with_borrow_mut(|rng| {
-        let len = rng.gen_range(STRING_SIZE);
+        let len = rng.gen_range(1..=50);
         Alphanumeric.sample_string(rng, len)
     })
 }
--- src/sort.rs
+++ src/sort.rs
@@ -14,10 +14,25 @@ impl Sort for Stdlib {
     }
 }
 
+pub struct Insertion;
+
+impl Sort for Insertion {
+    fn sort<T: Ord>(self, items: &mut [T]) {
+        for j in 1..items.len() {
+            for i in (1..=j).rev() {
+                if items[i - 1] > items[i] {
+                    items.swap(i - 1, i);
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::rand::*;
+    use std::{any, time::Instant};
 
     #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
     struct Custom {
@@ -25,30 +40,72 @@ mod tests {
         string: String,
     }
 
+    impl Custom {
+        fn random() -> Self {
+            Self {
+                int: range(1..5),
+                string: string(),
+            }
+        }
+    }
+
+    fn name<T>(t: &T) -> &'static str {
+        any::type_name_of_val(t).rsplit("::").next().unwrap()
+    }
+
     fn test<const N: usize>(sorter: impl Sort, mut items: [impl Ord; N]) {
+        println!("Sorter: {}", name(&sorter));
+        println!("Sorting: {}", name(&items[0]));
+        println!("N: {N}");
+
         assert!(!is_sorted(&items));
+        let before = Instant::now();
         sorter.sort(&mut items);
+        println!("Elapsed: {}µs", before.elapsed().as_micros());
+
         assert!(is_sorted(&items));
+        println!();
     }
 
     #[test]
-    fn stdlib_sort_ints() {
-        test(Stdlib, array_of(int));
+    fn stdlib_sort_int() {
+        test(Stdlib, ten_of(int));
+        test(Stdlib, hundred_of(int));
+        test(Stdlib, thousand_of(int));
     }
 
     #[test]
-    fn stdlib_sort_strings() {
-        test(Stdlib, array_of(string));
+    fn stdlib_sort_string() {
+        test(Stdlib, ten_of(string));
+        test(Stdlib, hundred_of(string));
+        test(Stdlib, thousand_of(string));
     }
 
     #[test]
     fn stdlib_sort_custom() {
-        test(
-            Stdlib,
-            array_of(|| Custom {
-                int: range(1..5),
-                string: string(),
-            }),
-        );
+        test(Stdlib, ten_of(Custom::random));
+        test(Stdlib, hundred_of(Custom::random));
+        test(Stdlib, thousand_of(Custom::random));
+    }
+
+    #[test]
+    fn insertion_sort_int() {
+        test(Insertion, ten_of(int));
+        test(Insertion, hundred_of(int));
+        test(Insertion, thousand_of(int));
+    }
+
+    #[test]
+    fn insertion_sort_string() {
+        test(Insertion, ten_of(string));
+        test(Insertion, hundred_of(string));
+        test(Insertion, thousand_of(string));
+    }
+
+    #[test]
+    fn insertion_sort_custom() {
+        test(Insertion, ten_of(Custom::random));
+        test(Insertion, hundred_of(Custom::random));
+        test(Insertion, thousand_of(Custom::random));
     }
 }
